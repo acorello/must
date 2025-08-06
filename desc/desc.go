9eef0f63a9ca8e77751952a6267e3dcf531cdf39
am <am@acorello.it>
2025-08-06 15:18:23 +0100

    Move must.Desc to desc.Err

--- desc/desc.go
+++ desc/desc.go
@@ -1,11 +1,27 @@
 package desc
 
-type String string
+import (
+	"fmt"
+	"strings"
+)
 
-func (me String) String() string {
+type str string
+
+func (me str) String() string {
 	return string(me)
 }
 
-func Str(s string) String {
-	return String(s)
+func Str(s string) str {
+	return str(s)
+}
+
+func Err(err error, context fmt.Stringer) error {
+	if err == nil {
+		return nil
+	}
+	var message = context.String()
+	if !strings.Contains(message, "%w") {
+		message = message + " ⁝ %w"
+	}
+	return fmt.Errorf(message, err)
 }
